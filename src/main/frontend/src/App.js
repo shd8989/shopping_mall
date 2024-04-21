26ad60ea045caa89c05acd2eeb0e4b7d94dc1f14
hyundong-seo <terranpush@naver.com>
2024-04-21 18:34:58 +0900

    #5 수정 - 부트스트랩 header, footer 적용

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {Route, Routes} from "react-router-dom";
 import axios from 'axios';
 import Header from './common/Header';
+import Footer from './common/Footer';
 import Page1 from './Page1';
 import Page2 from './Page2';
 
@@ -11,24 +12,6 @@ class App extends React.Component {
         this.state={};
     }
 
-/*
-   const [hello, setHello] = useState('');
-
-    useEffect(() => {
-        axios.get('/api/v1')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
-*/
-
-/*
-    return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
-    );
-    */
-
     render() {
         return (
           <div className="App">
@@ -37,6 +20,7 @@ class App extends React.Component {
                 <Route path="/page1" element={<Page1 />} />
                 <Route path="/page2" element={<Page2 />} />
             </Routes>
+            <Footer />
           </div>
         );
     }
--- src/main/frontend/src/common/Header.jsx
+++ src/main/frontend/src/common/Header.jsx
@@ -1,42 +1,53 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React from 'react';
 
 const Header = () => {
-    const [hello, setHello] = useState('');
-
-        useEffect(() => {
-            axios.get('http://localhost:8080/api/v1')
-            .then(response => setHello(response.data))
-            .catch(error => console.log(error))
-        }, []);
 
     return (
           <>
-            <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            {/* Navbar Brand*/}
-            <a className="navbar-brand ps-3" href="dbconn_list">Analyze Query</a>
-            {/* Sidebar Toggle*/}
-            <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i className="fas fa-bars" /></button>
-            {/* Navbar Search*/}
-            <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                <div className="input-group">
-                <input className="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                <button className="btn btn-primary" id="btnNavbarSearch" type="button"><i className="fas fa-search" /></button>
+            <header className="header-area header-sticky">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <nav className="main-nav">
+                                <div className='hidden-style'>Logo Start</div>
+                                <a href="index.html" className="logo">
+                                    <img src="assets/images/logo.png" />
+                                </a>
+
+                                <div className='hidden-style'>Menu Start</div>
+                                <ul className="nav">
+                                    <li className="scroll-to-section"><a href="#top" className="active">Home</a></li>
+                                    <li className="scroll-to-section"><a href="#men">Men's</a></li>
+                                    <li className="scroll-to-section"><a href="#women">Women's</a></li>
+                                    <li className="scroll-to-section"><a href="#kids">Kid's</a></li>
+                                    <li className="submenu">
+                                        <a href="javascript:;">Pages</a>
+                                        <ul>
+                                            <li><a href="about.html">About Us</a></li>
+                                            <li><a href="products.html">Products</a></li>
+                                            <li><a href="single-product.html">Single Product</a></li>
+                                            <li><a href="contact.html">Contact Us</a></li>
+                                        </ul>
+                                    </li>
+                                    <li className="submenu">
+                                        <a href="javascript:;">Features</a>
+                                        <ul>
+                                            <li><a href="#">Features Page 1</a></li>
+                                            <li><a href="#">Features Page 2</a></li>
+                                            <li><a href="#">Features Page 3</a></li>
+                                            <li><a rel="nofollow" href="https://templatemo.com/page/4" target="_blank">Template Page 4</a></li>
+                                        </ul>
+                                    </li>
+                                    <li className="scroll-to-section"><a href="#explore">Explore</a></li>
+                                </ul>
+                                <a class='menu-trigger'>
+                                    <span>Menu</span>
+                                </a>
+                            </nav>
+                        </div>
+                    </div>
                 </div>
-            </form>
-            {/* Navbar*/}
-            <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li className="nav-item dropdown">
-                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a className="dropdown-item" href="#!">Settings</a></li>
-                    <li><a className="dropdown-item" href="#!">Activity Log</a></li>
-                    <li><hr className="dropdown-divider" /></li>
-                    <li><a className="dropdown-item" href="#!">Logout</a></li>
-                </ul>
-                </li>
-            </ul>
-                백엔드에서 가져온 데이터입니다 : {hello}
-            </nav>
+            </header>
           </>
       );
 };
